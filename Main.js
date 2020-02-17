5707516776bdf1d701eecc23a686c5ff63ef231c
BenQuartermaine <ben.quartermaine3@gmail.com>
2020-02-17 19:05:31 +1100

    react component

---
+++ Main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Main = () => {
+  return (
+    <div style="background: red; color: blue">
+      <button>Hello</button>
+    </div>
+  )
+}
+
+export default Main;
